4ede739f68fb19a5336bd4e377120f38fdcea6ed
member.fog <member_fog@member.fog>
2024-03-02 14:20:58 -0600

    Design added to day highlights cards.

--- src/components/Highlights/Highlights.jsx
+++ src/components/Highlights/Highlights.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import humidity_icon from "./../../icons/humidity-icon.png";
+import humidity from "./../../icons/humidity.png";
+import pressure from "./../../icons/perssure.png";
+import sunrise from "./../../icons/sunrise.png";
+import sunset from "./../../icons/sunset.png";
+import wind from "./../../icons/wind.png";
+import "./Highlights.css";
 
 const Highlights = (props) => {
   return (
@@ -15,35 +21,56 @@ const Highlights = (props) => {
               : "day-info"
           }
         >
-          <div className="day-item-sunset">
-            {props.weatherConditions.sys === undefined
-              ? null
-              : `${new Date(
-                  props.weatherConditions.sys.sunrise * 1000
-                ).toLocaleTimeString("en-US")}, ${new Date(
-                  props.weatherConditions.sys.sunset * 1000
-                ).toLocaleTimeString("en-US")}`}
-          </div>
-          <div className="day-item-presure">
-            {props.weatherConditions.main === undefined
-              ? null
-              : `Pressure: ${props.weatherConditions.main.pressure}`}
-          </div>
-          <div className="day-item-humidity">
-            {props.weatherConditions.main === undefined ? null : (
+          <div className="day-item-sun">
+            {props.weatherConditions.sys === undefined ? null : (
               <>
-                <div className="humidity-title">
-                  <p>Humidity</p>
+                <div className="sun-container">
+                  <img src={sunrise} alt="" />
+                  <p>
+                    <span>Sunrise: </span>{" "}
+                    {new Date(
+                      props.weatherConditions.sys.sunrise * 1000
+                    ).toLocaleTimeString("en-US")}
+                  </p>
                 </div>
-                <img src={humidity_icon} alt="" />
-                <p className="humidity">{`${props.weatherConditions.main.humidity}%`}</p>
+                <div className="sun-container">
+                  <img src={sunset} className="sunrise" alt="" />
+                  <p>
+                    <span>Sunset: </span>{" "}
+                    {new Date(
+                      props.weatherConditions.sys.sunset * 1000
+                    ).toLocaleTimeString("en-US")}
+                  </p>
+                </div>
               </>
             )}
           </div>
+          <div className="day-item-pressure">
+            {props.weatherConditions.main === undefined ? null : (
+              <div className="pressure-container">
+                <img src={pressure} alt="" />
+                <p>
+                  <span>Air pressure: </span>{" "}
+                  {props.weatherConditions.main.pressure} Pa
+                </p>
+              </div>
+            )}
+          </div>
+          <div className="day-item-humidity">
+            {props.weatherConditions.main === undefined ? null : (
+              <div className="humidity-container">
+                <img src={humidity} className="humidity" alt="" />
+                <p>Humidity: {props.weatherConditions.main.humidity}%</p>
+              </div>
+            )}
+          </div>
           <div className="day-item-wind">
-            {props.weatherConditions.wind === undefined
-              ? null
-              : `Wind speed: ${props.weatherConditions.wind.speed}m/s`}
+            {props.weatherConditions.wind === undefined ? null : (
+              <div className="wind-container">
+                <img src={wind} className="wind" alt="" />
+                <p>Wind speed: {props.weatherConditions.wind.speed}km/h</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
